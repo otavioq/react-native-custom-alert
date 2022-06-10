070ba83815448ba25db6b7aeeb87bd45e5a09380
otavioq <otavioquinellato@hotmail.com>
2022-06-10 11:26:28 -0300

    Ios package adjustment

--- ios/RNSweetAlert-Bridging-Header.h
+++ ios/RNSweetAlert-Bridging-Header.h
@@ -1,10 +1,10 @@
 #ifndef RNSweetAlert_Bridging_Header_h
 #define RNSweetAlert_Bridging_Header_h
 
-#import React/RCTBridgeModule.h
-#import React/RCTEventEmitter.h
-#import React/RCTViewManager.h
-#import React/RCTConvert.h
-#import React/RCTUtils.h
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTConvert.h>
+#import <React/RCTUtils.h>
 
 #endif /* RNSweetAlert_Bridging_Header_h */
